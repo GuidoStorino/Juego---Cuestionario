a60a768af6f206660795e1477fd5a7207b55500e
Guido Storino <guido.storino@gmail.com>
2025-07-15 10:26:19 -0300

    pulpería

--- src/data/bosque.js
+++ src/data/bosque.js
@@ -13,11 +13,59 @@ export const bosque = {
   sendero_tierra: {
     texto: "Las hojas doradas comienzan a caer como una lluvia incesante. ¿Qué hacés?",
     opciones: [
-    { texto: "Seguir por el sendero. En algún momento pararán.", destino: "atula_estatua"}, 
+    { texto: "Seguir por el sendero. En algún momento pararán.", destino: "bares_bosque"}, 
     {texto: "Salirte del camino", destino: "cabana_bosque" }]
   },
 
-  campo_flores: {
+bares_bosque: {
+  texto: "Un poco más lejos y a ambos lados se llegan a ver dos lugares en donde te podrías cobijar por un rato. Uno parece una pulpería de la que te olvidás el nombre ni bien le quitás la vista. El otro es una casa de té.",
+  opciones: [
+    {texto:"Pulpería", destino: "pulperia"},
+    {texto: "Casa de té", destino: "casa_de_te"}
+  ]
+},
+
+pulperia: {
+  texto: "La pulpería ofrece buena comida y sus paredes están adornadas con cuadros de la época en la que fue construida y objetos antiguos. Al momento de irte, te ofrecen llevarte alguna.",
+  opciones: [
+    {texto: "Me llevo esa asombrosa espada que cuelga de la pared.", objeto: "espada", destino: "arturo"},
+    {texto: "No, gracias. Pero me voy a llevar la botella que me quedó un poco de vino", objeto: "botella", destino:"arturo"}
+  ]
+},
+
+arturo: {
+  texto: "Al salir del lugar, la lluvia de hojas se ha calmado. Sin embargo, luego de caminar un poco hay otro lugar que llama tu atención. Es un local pequeño atendido por un hombre entrado en años llamado Arturo, quien te ofrece algunas de las especias que él mismo hace, no sin antes hablarte sobre el lugar y advertirte de las sorpresas que te podés encontrar por el camino.",
+  opciones: [
+    {texto: "Llevás un dulce de leche. Para alguna merienda", destino: "atula_estatua"},
+    {texto: "Llevás una sal natural hecha con 14 especias diferentes", destino: "atula_estatua"}
+  ]
+},
+  
+
+  atula_estatua: {
+    texto: "Una estatua oscura y de aspecto femenino se ve a unos pasos. A sus pies yace una inscripción. Preferís que la inscripción diga:",
+    opciones: [
+      { texto: "Has encontrado la estatua de Atula. La contemplación de su figura y la lectura de este escrito la hará cobrar vida. Tu destino está marcado.", objeto: "espada", destino: "atula_vida" },
+      { texto: "Has encontrado la estatua de Atula. Bienaventurada seas. Tu fe en ella te ayudará a seguir por este bosque", destino: "pasos_bosque" }
+   ]
+  },
+  atula_vida: {
+    texto: "La inmovilidad de la estatua se convierte en una mujer enorme y de aspecto macabro. Inmediatamente te mira con ojos fijos y furiosos. De a poco, levanta una mano para atacarte con alguna magia. ¿Qué hacés en esta situación?",
+    opciones: [
+      {texto: "Desenvaino la espada que me llevé de La Protegida y se la clavó en el corazón. O donde pueda.", destino: "atula_espada", requiere: "espada"},
+      {texto: "Desesperada, le tiro la sal de Arturo en los ojos.", destino: "atula_sal"},
+      {texto: "Salgo corriendo. Y rezo porque corramos bien rápido", destino: "atula_correr"},
+      {texto: "Le parto la botella de vino en la cabeza. Total ya está vacía", destino: "atula_botella"}
+    ]
+  },
+
+  atula_espada: {
+    texto: "ganaste"
+  },
+
+
+
+    campo_flores: {
     texto: "El campo es realmente inmenso. Hay flores y más flores hacia todas partes.",
     opciones: [
       { texto: "Seguir caminando y mirando las flores", destino: "perderse" },
@@ -41,21 +89,6 @@ export const bosque = {
       ]
   },
 
-  
-
-  atula_estatua: {
-    texto: "Una estatua oscura y de aspecto femenino se ve a unos pasos. A sus pies yace una inscripción. Preferís que la inscripción diga:",
-    opciones: [
-      { texto: "Has encontrado la estatua de Atula. La contemplación de su figura y la lectura de este escrito la hará cobrar vida. Tu destino está marcado.", objeto: "espada", destino: "atula_vida" },
-      { texto: "Has encontrado la estatua de Atula. Bienaventurada seas. Tu fe en ella te ayudará a seguir por este bosque", destino: "pasos_bosque" }
-   ]
-  },
-  atula_vida: {
-    texto: "La inmovilidad de la estatua se convierte en una mujer enorme y de aspecto macabro. Inmediatamente te mira con ojos fijos y furiosos. De a poco, levanta una mano para atacarte con alguna magia. ¿Qué hacés en esta situación?",
-    opciones: [
-      {texto: "espada", destino: "espada", requiere: "espada"}
-    ]
-  },
 
   cabana_bosque: {
     texto: "Te agitaste y perdiste.",
