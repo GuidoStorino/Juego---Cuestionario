c6376eb999588c0aab145355f67b81b159a3cfba
GuidoStorino <guido.storino@gmail.com>
2025-08-28 19:10:52 -0300

    sweet 2

--- src/data/b_sw.js
+++ src/data/b_sw.js
@@ -93,9 +93,17 @@ chupada_sw: {
   texto: "No estaba del todo parada, pero adentro de tu boca se endurece muy rápido y empezás a hacer movimientos que la ponen todavía más gorda.",
   leche: 3,
   opciones: [
-    {texto: "Mirar cómo disfruto mientras me la chupás", destino: "chupada_acabada"},
-    {texto: "Empezar a pajearme para que me exite mientras miro alrededor", destino: "mirar_habitacion"},
+    {texto: "Mirar cómo disfruto mientras me la chupás", destino: "chupada_puerta_uno"},
+    {texto: "Empezar a pajearme para que me excite mientras miro alrededor", destino: "mirar_habitacion"},
     {texto: "Mirarme, sonreírme, volver a ponerme el pantalón y llevarme de la mano a seguir explorando el lugar", destino: "primer_piso"}
+  ]
+},
+
+mirar_habitacion: {
+  texto: "Me mirás mientras me pajeás y me sonreís, con una sonrisa que hasta tiene malicia. Sabés bien lo que me gusta. Me seguís pajeando, mientras miro, mientras me hablás y te leo los labios, mientras... Uff...",
+  leche: 4,
+  opciones: [
+    {texto: "Salir de la habitación", destino: "primer_piso"}
   ]
 },
 
@@ -108,6 +116,117 @@ beso_sw: {
     {texto: "Te saco la remera, que se toquen viendo esas tetas a punto de explotarte adentro del corpiño", destino: "tetas_puerta_dos"}
   ]
 
+},
+
+chupada_puerta_uno: {
+  texto: "La seguís chupando con los ojitos mirando para arriba. Te acaricio un poco la cabeza.",
+  leche: 2,
+  opciones: [
+    {texto: "Meter un pongo de lengua y de velocidad", destino: "chupada_lengua"},
+    {texto: "Levantarte, agarrarme de la mano y llevarme a un sillón", destino: "sillon_puerta_uno"},
+    {texto: "Levantarte, dejarme caliente, agarrarme de la mano y salir de la habitación", destino: "primer_piso"}
+  ]
+},
+
+chupada_lengua: {
+  texto: "La pija se me pone más dura, ahora cerraste los ojos y te concentrás. Te agarro la cabeza más fuerte y disfruto.",
+  leche: 3,
+  opciones: [
+    {texto: "Seguir", destino: "chupada_acabada"},
+    {texto: "Volver a mirarme, sonreír, subirme el pantalón y salir de la habitación", destino: "primer_piso"}
+  ]
+},
+
+chupada_acabada: {
+  texto: "Te la di toda en la boca",
+  leche: 4,
+  opciones: [
+    {texto: "Salir de la habitación", destino: "primer_piso"},
+    {texto: "Salir del boliche", destino: "calle_inicio"}
+  ]
+},
+
+mirar_coger: {
+  texto: "La mina parece estar pasándola bien mientras se la dan toda desde atrás. Me tocás un poco la pija por sobre el pantalón y me decís cosas en el oído",
+  leche: 2,
+  opciones: [
+    {texto: "Me pedís que te haga lo mismo y me llevás contra una pared", destino: "coger_pared"},
+    {texto: "Te agachás y me la empezás a chupar ahí mismo", destino: "chupada_sw"},
+    {texto: "Bueno, basta de mirar por acá, vamos a ver el lugar un poco más", destino: "primer_piso"}
+  ]
+},
+
+coger_pared: {
+  texto: "Te empiezo a dar contra la pared y empiezo a gemir de cómo me gusta. Te entra de una manera hermosa. Estamos mojados los dos, te agarro una teta desde atrás.",
+  leche: 3,
+  opciones: [
+    {texto: "Ya fue. Te sigo dando hasta no dar más", destino: "acabar_pared"},
+    {texto: "Paremos un poco. La noche no termina acá", destino: "primer_piso"}
+  ]
+},
+
+acabar_pared: {
+  texto: "Me habría quedado rebotando en tu cola toda la noche si hubiera podido.",
+  leche: 4,
+  opciones: [
+    {texto: "Volvamos a la pista de baile", destino: "baile_sw"},
+    {texto: "Salgamos de la habitación", destino: "primer_piso"}
+  ]
+},
+
+montar_sw: {
+  texto: "Te empezás a mover de una manera hermosa, no hay nada que me guste más que lo que me estás haciendo. Te miro a la cara y te beso, te toco un poco las tetas. Siento la pija bien dura adentro de tu concha que no se para de mover",
+  leche: 3,
+  opciones: [
+    {texto: "Seguir montándome", destino: "acabada_montar"},
+    {texto: "Te cansás un poco... Ahora vos vas al sillón", destino: "swing_1"},
+    {texto: "Salir de la habitación", destino: "primer_piso"}
+  ]
+},
+
+acabada_montar: {
+  texto: "Te tuve que empezar a hacer gestos, a tocarte, apretarte, para que salieras de ahí (por más que no quisiera).",
+  leche: 4,
+  opciones: [
+    {texto: "Ir a la pista", destino: "baile_sw"},
+    {texto: "Salir del boliche", destino: "calle_inicio"}
+  ]
+},
+
+swing_1: {
+  texto: "Te recostás sobre el sillón, abrís las piernas y te empiezo a dar. En eso, una pareja viene y empieza a hacer lo suyo. El que está acostado es él. Ella, en cuatro, se la chupa. Él te mira y te empieza a tocar un poco las tetas",
+  leche: 3,
+  opciones: [
+    {texto: "Me preguntás si está todo bien, y me decís que yo también puedo tocar", destino: "swing_2"},
+    {texto: "No te gusta esto. Agarramos nuestra ropa y nos vamos", destino: "primer_piso"}
+  ]
+},
+
+swing_2: {
+  texto: "Te sigue tocando las tetas por encima de la ropa mientras busca cómo sacártela. Yo le manoseo el orto a la pareja, por encima de su tanga blanca.",
+  leche: 3,
+  opciones: [
+    {texto: "Hasta acá llegamos. Te levantás, agarrás la ropa, le decís 'gracias' y nos vamos", destino: "primer_piso"},
+    {texto: "Te levantás un poco la ropa para quedarte en corpiño mientras yo le sigo tocando el culo a la mina y un poco más abajo también", destino: "swing_3"}
+  ]
+},
+
+swing_3: {
+  texto: "Tenés mi pija adentro y te estimulan los pezones. Yo te doy mientras le acaricio la conchita a la que tengo al lado.",
+  leche: 3,
+  opciones: [
+    {texto: "Seguir disfrutando, la estamos pasando bien", destino: "swing_4"},
+    {texto: "Estuvo bueno, pero mejor vamos a otro lado", destino: "primer_piso"},
+    ]
+},
+
+swing_4: {
+  texto: "Menos mal que te levantaste la ropa porque si no te la habría manchado toda. Aunque ese corpiño rojo ahora tiene un poco de blanco",
+  leche: 4,
+  opciones: [
+    {texto: "Salir de la habitación, no sin antes darle las gracias a la pareja, obvio", destino: "primer_piso"},
+    {texto: "Salir del boliche", destino: "calle_inicio"}
+  ]
 },
 
 besos_puerta_dos: {
